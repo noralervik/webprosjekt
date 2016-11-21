1936acecca3ec253e03903b7bd200788a3e4fd44
noralervik <noralervik@gmail.com>
2016-11-21 15:03:08 +0100

    Bildeslide og logostr
    La inn forskjellige bilder i slideshowene på de forskjellige sidene

--- javascriptfilHytter.js
+++ javascriptfilHytter.js
@@ -1,26 +1,26 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
 }
 
-function showSlides(n) {
+function showSlides2(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides2");
   var dots = document.getElementsByClassName("dot2");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex2-1].style.display = "block";
+  dots[slideIndex2-1].className += " active";
 }
