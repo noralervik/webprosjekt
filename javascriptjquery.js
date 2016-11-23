732fcbe88deac68d1caaebdb78ffdef6ca4b40b0
reginebk <regine_bk@hotmail.com>
2016-11-23 10:49:11 +0100

    Kommentar i jsJQuery og endring i bilder omoss

--- javascriptjquery.js
+++ javascriptjquery.js
@@ -1,3 +1,5 @@
+//Her lager vi den ekstra effekt på bildene inne på Bestillingsiden. Slik
+//at bilde blir klar når vi tar musepekeren over.
 $(document).ready(function(){
   $(".midtstilt").bind("mouseover", function() {
     $(this).css("opacity", "1");
